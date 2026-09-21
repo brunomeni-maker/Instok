bc92500de2d7ffa21a46eded83011191e94772cc
brunomeni-maker <bruno.meni@aluno.lsb.com.br>
2026-09-21 09:39:11 -0300

    fix: corrige dominio nos arquivos SEO

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/robots.txt", (req, res) => {
 `User-agent: *
 Allow: /
 
-Sitemap: https://instok-jj72.onrender.com/sitemap.xml`
+Sitemap: https://instok-ji72.onrender.com/sitemap.xml`
   );
 });
 
@@ -23,7 +23,7 @@ app.get("/sitemap.xml", (req, res) => {
 `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://instok-jj72.onrender.com/</loc>
+    <loc>https://instok-ji72.onrender.com/</loc>
   </url>
 </urlset>`
   );
@@ -47,7 +47,7 @@ Instok é uma aplicação que ajuda usuários a encontrar produtos disponíveis 
 
 ## Site
 
-https://instok-jj72.onrender.com/`
+https://instok-ji72.onrender.com/`
   );
 });
 app.use(express.static("."));
